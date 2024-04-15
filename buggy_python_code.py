cafeaefb36e0e4f2782ba107c88b8ea3c3228135
Miroslav Patlevič <524936@mail.muni.cz>
2024-04-15 10:46:39 +0200

    Fix codeQL

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -23,8 +23,9 @@ def print_nametag(format_string, person):
 
 def fetch_website(urllib_version, url):
     # Import the requested version (2 or 3) of urllib
-    exec(f"import urllib{urllib_version} as urllib", globals())
-    # Fetch and print the requested URL
+    if len([for section in urllib_version.split('.') if section.isdigit()]) == 3:
+        exec(f"import urllib{urllib_version} as urllib", globals())
+        # Fetch and print the requested URL
  
     try: 
         http = urllib.PoolManager()
